def14c8565b78ba996af64f807054a525e6b3d52
Emily Bozich <ebozich@login2.hoffman2.idre.ucla.edu>
2025-04-10 14:27:58 -0700

    Syntax fixes

--- scripts/distances/rearrange_distance_metric_dicts.py
+++ scripts/distances/rearrange_distance_metric_dicts.py
@@ -9,12 +9,12 @@ def main():
     interactome_source = 'PathFX'
     threshold = 0.50
     interactome_aname = interactome_source + '_' + str(threshold) + 'thr_int'
-    exper_source = 'DREAM'
+    exper_source = 'NCI_ALMANAC'
 
-    target_type = 'single' #'multi'
+    target_type = 'multi' #'single' #'multi'
 
     #Load distance dictionaries
-    distance_results_dir = os.path.join('../../results/distance_dictionaries', exper_source, interactome_aname)
+    distance_results_dir = os.path.join('../../results/distance_dictionaries', interactome_aname, exper_source)
 
     sp_filename = target_type + '_target_SP_dict.pkl'
     t2t_dist_filename = target_type + '_target_t2t_diffusion_dist_0.06-0.75_alphaD_dict.pkl'
@@ -29,23 +29,30 @@ def main():
     #Initializes combined dictionaries
     distances = ['T1_to_T2', 'T2_to_T1', 'T1_to_N2', 'T2_to_N1', 'N1_to_N2', 'N2_to_N1']
     alphaDs = list(t2t_dist_dict.keys())
-    pairwise_combos = t2t_dist_dict[alphaDs[0]].keys()
+    #pairwise_combos = t2t_dist_dict[alphaDs[0]].keys()
+    pairwise_combos = [combo for combo in sp_dict.keys() if not math.isnan(sp_dict[combo])]
     combined_distance_dicts = {d: {a: {c: [] for c in pairwise_combos} for a in alphaDs} for d in distances}
     combined_distance_dicts.update({'ASP': {0: {c: [] for c in pairwise_combos}}})
 
     for a in alphaDs:
         for combo in pairwise_combos:
 
-            combined_distance_dicts['ASP'][0][combo] = sp_dict[combo] #ASP doesn't use alphaD, using 0 as a placeholder
+            asp_dist = sp_dict[combo]
 
-            combined_distance_dicts['T1_to_T2'][a][combo] = t2t_dist_dict[a][combo][0]
-            combined_distance_dicts['T2_to_T1'][a][combo] = t2t_dist_dict[a][combo][1]
+            if math.isnan(asp_dist):
+                continue
+            else:
 
-            combined_distance_dicts['T1_to_N2'][a][combo] = t2n_dist_dict[a][combo][0]
-            combined_distance_dicts['T2_to_N1'][a][combo] = t2n_dist_dict[a][combo][1]
+                combined_distance_dicts['ASP'][0][combo] = sp_dict[combo] #ASP doesn't use alphaD, using 0 as a placeholder
 
-            combined_distance_dicts['N1_to_N2'][a][combo] = n2n_dist_dict[a][combo][0]
-            combined_distance_dicts['N2_to_N1'][a][combo] = n2n_dist_dict[a][combo][1]
+                combined_distance_dicts['T1_to_T2'][a][combo] = t2t_dist_dict[a][combo][0]
+                combined_distance_dicts['T2_to_T1'][a][combo] = t2t_dist_dict[a][combo][1]
+
+                combined_distance_dicts['T1_to_N2'][a][combo] = t2n_dist_dict[a][combo][0]
+                combined_distance_dicts['T2_to_N1'][a][combo] = t2n_dist_dict[a][combo][1]
+
+                combined_distance_dicts['N1_to_N2'][a][combo] = n2n_dist_dict[a][combo][0]
+                combined_distance_dicts['N2_to_N1'][a][combo] = n2n_dist_dict[a][combo][1]
 
     #Save results
     combined_dist_dicts_filename = target_type + '_target_37_dist_metric_dicts.pkl'
@@ -53,6 +60,6 @@ def main():
 
 
 if __name__ == "__main__":
-    import os, pickle, functools
+    import os, pickle, functools, math
     from collections import defaultdict
     main()
